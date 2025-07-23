6d9a49f1d26a597b7bb62f334d49f5fd2bb93519
mohama123 <mohama123@medtronic.com>
2025-07-23 18:26:42 -0500

    feat: simple get request using net/http pkg #3

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprint(w, "Hello world")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	println("hello, world!")
+	// Registers HandleRoot func to handle requests to "/"
+	http.HandleFunc("/", HandleRoot)
+	// Starts the HTTP server on port 8080 and runs until it gets an error 
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
